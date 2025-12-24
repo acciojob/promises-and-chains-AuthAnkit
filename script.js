a806464e54c90578b5f5ccfa6f84f239b9280318
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-12-25 00:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const form = document.getElementById("formageandname");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // stop page reload
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+
+  // Validation
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Promise creation
+  const votePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 4000);
+  });
+
+  // Promise handling
+  votePromise
+    .then((username) => {
+      alert(`Welcome, ${username}. You can vote.`);
+    })
+    .catch((username) => {
+      alert(`Oh sorry ${username}. You aren't old enough.`);
+    });
+});
